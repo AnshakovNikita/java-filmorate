d4f0b5d2d1842eee98031b45dded29621b4f9dca
AnshakovNikita <kubishe1488@yandex.ru>
2022-08-05 17:26:04 +0300

    реализации ТЗ8

---
+++ src/test/java/ru/yandex/practicum/filmorate/controllers/UserControllerTest.java
@@ -0,0 +1,63 @@
+package ru.yandex.practicum.filmorate.controllers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.controller.UserController;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+public class UserControllerTest {
+    UserController userController = new UserController();
+    User testUser = new User();
+
+    @BeforeEach
+    void filmConfig() throws ValidationException {
+        testUser.setEmail("student@yandex.ru");
+        testUser.setLogin("Student");
+        testUser.setName("Никита");
+        testUser.setBirthday(LocalDate.of(1992, 3, 21));
+        userController.addUser(testUser);
+    }
+
+    @Test
+    public void shouldBeEmpty() throws ValidationException {
+        userController.addUser(testUser);
+        assertFalse(userController.getUsers().isEmpty());
+    }
+
+    @Test
+    void emailIsEmpty() {
+        testUser.setEmail("");
+        Exception exception = assertThrows(ValidationException.class, () -> userController.addUser(testUser));
+        assertEquals("email не может быть пустым и должен содержать @", exception.getMessage());
+
+        testUser.setEmail("studentyandex.ru");
+        Exception ex = assertThrows(ValidationException.class, () -> userController.addUser(testUser));
+        assertEquals("email не может быть пустым и должен содержать @", ex.getMessage());
+    }
+
+    @Test
+    void loginIsEmpty() {
+        testUser.setLogin("");
+        Exception exception = assertThrows(ValidationException.class, () -> userController.addUser(testUser));
+        assertEquals("login не может быть пустым и содержать пробелы", exception.getMessage());
+
+        testUser.setLogin("Stu dent");
+        Exception ex = assertThrows(ValidationException.class, () -> userController.addUser(testUser));
+        assertEquals("login не может быть пустым и содержать пробелы", ex.getMessage());
+    }
+
+    @Test
+    void nameIsEmpty() throws ValidationException {
+        User testUserNotName = new User();
+        testUserNotName.setEmail("student@yandex.ru");
+        testUserNotName.setLogin("Student");
+        testUserNotName.setName("");
+        testUserNotName.setBirthday(LocalDate.of(1992, 3, 21));
+        userController.addUser(testUserNotName);
+        assertEquals(testUserNotName.getLogin(), testUserNotName.getName());
+    }
+}
