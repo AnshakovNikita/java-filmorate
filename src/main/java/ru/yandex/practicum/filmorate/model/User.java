1bd5d77e1be7d698e3095d6f0f99540600104684
AnshakovNikita <kubishe1488@yandex.ru>
2022-08-06 11:30:07 +0300

    исправил замечания

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,14 +1,31 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+import lombok.NonNull;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 
 @Data
 public class User {
+    @NotNull
     private long id;
+
+    @NotNull
+    @Email
+    @Pattern(regexp = "^\\S*$")
     private String email;
+
+    @NotBlank
+    @Pattern(regexp = "^\\S*$")
     private String login;
+
+    @NotBlank
     private String name;
+
+    @NotNull
     private LocalDate birthday;
 }
